8de135e0c96cb98a8e6a1aa3b19416f8b146b98b
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 13:39:00 -0300

    Plugner-Patch-5:
    Created 2 new classes, to improve the plugin's developer experience, but without affect how the old plugins works.
    
    Classes:
    
     cn/nukkit/exception/UnknownConfigurationFileException (Exception if Configuration does not exists in getConfigurationSection)
     cn/nukkit/exception/UnknownConfigurationSectionException (Exception if SectionName does not exists in getConfigurationSection)
    
     cn/nukkit/PluginBase (Created new method getConfigurationSection)
    
     Sorry for my bad english.

---
+++ src/main/java/cn/nukkit/exception/UnknownConfigurationFileException.java
@@ -0,0 +1,7 @@
+package cn.nukkit.exception;
+
+public class UnknownConfigurationFileException extends Exception {
+    public UnknownConfigurationFileException(String message) {
+
+    }
+}
---
+++ src/main/java/cn/nukkit/exception/UnknownConfigurationSectionException.java
@@ -0,0 +1,7 @@
+package cn.nukkit.exception;
+
+public class UnknownConfigurationSectionException extends Exception {
+    public UnknownConfigurationSectionException(String message) {
+
+    }
+}
