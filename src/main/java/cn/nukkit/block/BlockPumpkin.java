1f8aecc6332c2dc8ffefc61632c9f14a725c009b
Dmitriy <dimonpo96@mail.ru>
2021-05-30 14:19:34 +0600

    -1 to 0

--- src/main/java/cn/nukkit/block/BlockPumpkin.java
+++ src/main/java/cn/nukkit/block/BlockPumpkin.java
@@ -28,7 +28,7 @@ import javax.annotation.Nonnull;
 public class BlockPumpkin extends BlockSolidMeta implements Faceable {
 	
 	private static final BlockPattern IRONGOLEM_PATTERN = new BlockPattern(
-        new BlockPattern.PatternBlock(PUMPKIN, -1, 1, 0),
+        new BlockPattern.PatternBlock(PUMPKIN, 0, 1, 0),
         new BlockPattern.PatternBlock(IRON_BLOCK, 0, 0, 1),
         new BlockPattern.PatternBlock(IRON_BLOCK, 0, 1, 1),
         new BlockPattern.PatternBlock(IRON_BLOCK, 0, 2, 1),
@@ -36,7 +36,7 @@ public class BlockPumpkin extends BlockSolidMeta implements Faceable {
     );
 
     private static final BlockPattern SNOWMAN_PATTERN = new BlockPattern(
-        new BlockPattern.PatternBlock(PUMPKIN, -1, 0, 0),
+        new BlockPattern.PatternBlock(PUMPKIN, 0, 0, 0),
         new BlockPattern.PatternBlock(SNOW_BLOCK, 0, 0, 1),
         new BlockPattern.PatternBlock(SNOW_BLOCK, 0, 0, 2)
     );
